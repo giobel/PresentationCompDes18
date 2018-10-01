58d208f7d39a51d26d14bfc6650bf28899f29c7f
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2018-10-01 22:27:37 +1000

    loader

--- compDes18Pres-export/js/APP_INIT.js
+++ compDes18Pres-export/js/APP_INIT.js
@@ -20,23 +20,31 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //THE SOFTWARE.
 
+
+
 //contains the jquery document.ready callback, which starts the application
 
+//a global variable to store our running Spectacles App
+var mySpectacles;
+
+//fires when everything has loaded
 $(document).ready(function(){
 
-
-    //load our sample JSON file
-    $.getJSON("./js/Spectacles_2.json", function( data ){
+    //load our sample JSON file from disk
+    $.getJSON("js/Spectacles_2.json", function( data ){
 
         //once loaded, initialize a Spectacles viewer by passing in the div to bind to, the json data, and a callback function
         //where we can enable application functionality in nice clean chunks
-        var mySpectacles = new SPECTACLES($("#Spectacles_output"), data, function(app){
+        mySpectacles = new SPECTACLES($("#Spectacles_output"), data, function(app){
 
             //call the UI / functionality modules
             app.userInterface();
+            app.openLocalFiles();
+            app.sceneUI();
+            app.lightingUI();
+            app.viewAndSelectionUI();
+            app.viewsUI();
             app.layersUI();
-            app.setBackgroundColor(0xD9D8D8);
         });
     });
-
 });
